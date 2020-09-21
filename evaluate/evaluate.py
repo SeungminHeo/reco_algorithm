2b69a703b37990f893a4ddadd2192242859fd45a
Baek <b970311@yonsei.ac.kr>
2020-09-22 00:22:57 +0900

    fix evaluate test code

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -20,44 +20,43 @@ class evaluate:
 
     # MAE
     def MAE(self):
-        self.logger.info('MAE')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         sum_rating = 0  # P-R 절대값 합
         num_rating = len(self.truth)
         for r, p in zip(self.truth, self.predict):
             sum_rating += abs(p - r)
+        self.logger.info('MAE : %f' %(sum_rating / num_rating))
         return sum_rating / num_rating
 
     # MSE
     def MSE(self):
-        self.logger.info('MSE')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         sum_rating = 0  # P-R 절대값의 제곱 합
         num_rating = len(self.truth)
         for r, p in zip(self.truth, self.predict):
             sum_rating += abs(p - r) ** 2
+        self.logger.info('MSE : %f' %(sum_rating / num_rating))
         return sum_rating / num_rating
 
     # RMSE
     def RMSE(self):
-        self.logger.info('RMSE')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         sum_rating = 0  # P-R 절대값의 제곱 합
         num_rating = len(self.truth)
         for r, p in zip(self.truth, self.predict):
             sum_rating += abs(p - r) ** 2
+        self.logger.info('RMSE : %f' %((sum_rating / num_rating) ** (1 / 2)))
         return (sum_rating / num_rating) ** (1 / 2)
 
     # AP@k
     def AP_k(self, k):
-        self.logger.info('AP@K')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         ap_sum = 0
@@ -70,28 +69,34 @@ class evaluate:
                 ap_sum += true_count / (idx + 1)
             if k == 0:
                 break
+        self.logger.info('AP@%f : %f' %(k, ap_sum / m))
         return ap_sum / m
 
     # DCG
     def DCG(self):
-        self.logger.info('DCG')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         dcg = 0.0
         for i, r in enumerate(self.predict):
             if r in self.truth:
                 dcg += 1.0 / math.log(i + 2, 2)
+        self.logger.info('DCG : %f' %(dcg))
         return dcg
 
     # NDCG
     def NDCG(self):
-        self.logger.info('NDCG')
-        if self.truth is None or self.predict is None:
+        if not hasattr(self, "truth") or not hasattr(self, "predict"):
             self.logger.error("don't set dataset")
             return -1
         idcg = 0.0
         len_truth = len(self.truth)
         for i in range(len_truth):
             idcg += 1.0 / math.log(i + 2, 2)
-        return self.DCG()/idcg
+            
+        dcg = 0.0
+        for i, r in enumerate(self.predict):
+            if r in self.truth:
+                dcg += 1.0 / math.log(i + 2, 2)
+        self.logger.info('NDCG : %f' %(dcg/idcg)) 
+        return dcg/idcg
---
+++ test/evaluate_test.py
@@ -0,0 +1,29 @@
+import math
+import unittest
+
+import sys
+sys.path.append("../evaluate")
+
+from evaluate import evaluate
+
+
+class evaluate_test(unittest.TestCase):
+    def setUp(self):
+        self.eval = evaluate()
+
+    def test_case1(self):
+        gt = [1, 2, 3, 4, 5]
+        predict = [3, 1, 8, 9, 4]
+        self.eval.set_data(gt, predict)
+        self.eval.MAE()
+        self.eval.MSE()
+        self.eval.RMSE()
+        self.eval.AP_k(10)
+        self.eval.NDCG()
+
+    def test_case2(self):
+        self.eval.NDCG()
+
+
+if __name__ == '__main__':
+    unittest.main()
