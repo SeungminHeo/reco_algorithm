5c946e6ed0a08085161344a16c47235c342470e7
Baek <b970311@yonsei.ac.kr>
2020-09-24 01:51:15 +0900

    Fix pep8

--- model/als.py
+++ model/als.py
@@ -1,11 +1,10 @@
 import logging
 import logging.config
-
 import yaml
-from easydict import EasyDict as dict_to_class
-
+from easydict import EasyDict
 import numpy as np
 logging.config.fileConfig('../conf/logging.conf')
+
 
 class ALS:
     def __init__(self):
@@ -24,42 +23,42 @@ class ALS:
         self.logger.info('start ALS ')
         self.logger.info('ALS parameters -> (r_lambda : %f), (nf : %f), (alpha : %f), (iteration : %f)' %(self.r_lambda, self.nf, self.alpha, self.iteration))
 
-    def set_data(self, R):  # R : array(user - item)
-        self.nu = R.shape[0]
-        self.ni = R.shape[1]
+    def set_data(self, r):  # R : array(user - item)
+        self.nu = r.shape[0]
+        self.ni = r.shape[1]
 
         # initialize X and Y with very small values
-        self.X = np.random.rand(self.nu, self.nf) * 0.01 # user's latent factor
-        self.Y = np.random.rand(self.ni, self.nf) * 0.01 # item's latent factor
+        self.X = np.random.rand(self.nu, self.nf) * 0.01  # user's latent factor
+        self.Y = np.random.rand(self.ni, self.nf) * 0.01  # item's latent factor
 
-        self.P = np.copy(R) # Binary Rating matrix
+        self.P = np.copy(r)  # Binary Rating matrix
         self.P[self.P > 0] = 1
-        self.C = 1 + self.alpha * R # Confidence matrix
+        self.C = 1 + self.alpha * r  # Confidence matrix
 
-    def _loss_function(self, xTy): # xTy : predict
-        predict_error = np.square(self.P - xTy)
+    def _loss_function(self, predict):  # xTy : predict
+        predict_error = np.square(self.P - predict)
         confidence_error = np.sum(self.C * predict_error)
         regularization = self.r_lambda * (np.sum(np.square(self.X)) + np.sum(np.square(self.Y)))
         total_loss = confidence_error + regularization
         return np.sum(predict_error), confidence_error, regularization, total_loss
 
     def _optimize_user(self):
-        yT = np.transpose(self.Y)
+        yt = np.transpose(self.Y)
         for u in range(self.nu):
-            Cu = np.diag(self.C[u])
-            yT_Cu_y = np.matmul(np.matmul(yT, Cu), self.Y)
-            lI = np.dot(self.r_lambda, np.identity(self.nf))
-            yT_Cu_pu = np.matmul(np.matmul(yT, Cu), self.P[u])
-            self.X[u] = np.linalg.solve(yT_Cu_y + lI, yT_Cu_pu)
+            cu = np.diag(self.C[u])
+            yt_cu_y = np.matmul(np.matmul(yt, cu), self.Y)
+            li = np.dot(self.r_lambda, np.identity(self.nf))
+            yt_cu_pu = np.matmul(np.matmul(yt, cu), self.P[u])
+            self.X[u] = np.linalg.solve(yt_cu_y + li, yt_cu_pu)
 
     def _optimize_item(self):
-        xT = np.transpose(self.X)
+        xt = np.transpose(self.X)
         for i in range(self.ni):
-            Ci = np.diag(self.C[:, i])
-            xT_Ci_x = np.matmul(np.matmul(xT, Ci), self.X)
-            lI = np.dot(self.r_lambda, np.identity(self.nf))
-            xT_Ci_pi = np.matmul(np.matmul(xT, Ci), self.P[:, i])
-            self.Y[i] = np.linalg.solve(xT_Ci_x + lI, xT_Ci_pi)
+            ci = np.diag(self.C[:, i])
+            xt_ci_x = np.matmul(np.matmul(xt, ci), self.X)
+            li = np.dot(self.r_lambda, np.identity(self.nf))
+            xt_ci_pi = np.matmul(np.matmul(xt, ci), self.P[:, i])
+            self.Y[i] = np.linalg.solve(xt_ci_x + li, xt_ci_pi)
 
     def train(self):
         predict_errors = []
--- test/als_test.py
+++ test/als_test.py
@@ -1,37 +1,35 @@
-import math
 import unittest
-
 import sys
-sys.path.append("../model")
-
 import numpy as np
+sys.path.append("../model")
 from als import ALS
 
-class evaluate_test(unittest.TestCase):
+
+class Test(unittest.TestCase):
     def setUp(self):
         self.als = ALS()
 
     def test_case1(self):
-         R = np.array([[0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                  [0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0],
-                  [0, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0],
-                  [0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
-                  [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5],
-                  [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4],
-                  [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
-                  [0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0]])
-         self.als.set_data(R)
-         predict = self.als.train()
+        r = np.array([[0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                      [0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0],
+                      [0, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0],
+                      [0, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
+                      [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5],
+                      [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4],
+                      [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
+                      [0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0]])
+        self.als.set_data(r)
+        predict = self.als.train()
 
     def test_case2(self):
-         R = np.array([[0, 0, 1],
-                  [0, 0, 0],
-                  [0, 3, 4],
-                  [5, 8, 9]])
-         self.als.set_data(R)
-         predict = self.als.train()
+        r = np.array([[0, 0, 1],
+                      [0, 0, 0],
+                      [0, 3, 4],
+                      [5, 8, 9]])
+        self.als.set_data(r)
+        predict = self.als.train()
         
 
 if __name__ == '__main__':
