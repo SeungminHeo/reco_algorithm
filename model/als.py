6800d29d2a12a512baf0a9b3e27b84afade49f99
Baek <b970311@yonsei.ac.kr>
2020-10-02 15:00:33 +0900

    change matrix -> sparse for speed

--- model/als.py
+++ model/als.py
@@ -1,6 +1,6 @@
 from easydict import EasyDict
 import numpy as np
-
+from tqdm import tqdm
 
 
 class ALS:
@@ -9,6 +9,7 @@ class ALS:
             Implementation of Collaborative Filtering for Implicit Feedback datasets.
 
             Reference: http://yifanhu.net/PUB/cf.pdf"""
+
     def __init__(self, model_config, logger):
         '''
             model_config : config.yml['model_config']['als']
@@ -20,70 +21,87 @@ class ALS:
         self.alpha = params.get('alpha')
         self.iteration = params.get('iteration')
 
-        self.predict_errors = []
-        self.confidence_errors = []
-        self.regularization_list = []
-        self.total_losses = []
-
         self.logger = logger
         self.logger.info('start ALS ')
-        self.logger.info('ALS parameters -> (r_lambda : %f), (nf : %f), (alpha : %f), (iteration : %f)' %(self.r_lambda, self.nf, self.alpha, self.iteration))
+        self.logger.info('ALS parameters -> (r_lambda : %f), (nf : %f), (alpha : %f), (iteration : %f)' % (
+        self.r_lambda, self.nf, self.alpha, self.iteration))
 
-    def set_data(self, r):  # r : array(user - item)
+    def set_data(self, r):  # r : sparse array(user - item)
         self.nu = r.shape[0]
         self.ni = r.shape[1]
 
         # initialize X and Y with very small values
-        self.X = np.random.rand(self.nu, self.nf) * 0.01  # user's latent factor
-        self.Y = np.random.rand(self.ni, self.nf) * 0.01  # item's latent factor
+        self.X = sparse.csr_matrix(np.random.rand(self.nu, self.nf) * 0.01)  # user's latent factor
+        self.Y = sparse.csr_matrix(np.random.rand(self.ni, self.nf) * 0.01)  # item's latent factor
 
-        self.P = np.copy(r)  # Binary Rating matrix
-        self.P[self.P > 0] = 1
-        self.C = 1 + self.alpha * r  # Confidence matrix
+        # self.P = np.copy(r)  # Binary Rating matrix
+        # self.P[self.P > 0] = 1
+        # self.C = 1 + self.alpha * r  # Confidence matrix
+        self.conf = self.alpha * r
+        self.X_eye = sparse.eye(self.nu)
+        self.Y_eye = sparse.eye(self.ni)
+        self.lambda_eye = self.r_lambda * sparse.eye(self.nf)
 
+    '''
     def _loss_function(self, predict):
         predict_error = np.square(self.P - predict)
         confidence_error = np.sum(self.C * predict_error)
         regularization = self.r_lambda * (np.sum(np.square(self.X)) + np.sum(np.square(self.Y)))
         total_loss = confidence_error + regularization
         return np.sum(predict_error), confidence_error, regularization, total_loss
+    '''
 
     def _optimize_user(self):
-        yT = np.transpose(self.Y)
-        for u in range(self.nu):
-            cu = np.diag(self.C[u])
-            yT_cu_y = np.matmul(np.matmul(yT, cu), self.Y)
-            li = np.dot(self.r_lambda, np.identity(self.nf))
-            yT_cu_pu = np.matmul(np.matmul(yT, cu), self.P[u])
-            self.X[u] = np.linalg.solve(yT_cu_y + li, yT_cu_pu)
+        yT = self.Y.T
+        for u in tqdm(range(self.nu)):
+            conf_samp = self.conf[u, :].toarray()
+            P = conf_samp.copy()
+            P[P > 0] = 1
+            cu = sparse.diags(conf_samp, [0]) + self.Y_eye
+            yT_cu_y = yT.dot(cu).dot(self.Y)
+            yT_cu_pu = yT.dot(cu).dot(P.T)
+            self.X[u] = spsolve(yT_cu_y + self.lambda_eye, yT_cu_pu)
 
     def _optimize_item(self):
         xT = np.transpose(self.X)
         for i in range(self.ni):
-            ci = np.diag(self.C[:, i])
-            xT_ci_x = np.matmul(np.matmul(xT, ci), self.X)
-            li = np.dot(self.r_lambda, np.identity(self.nf))
-            xT_ci_pi = np.matmul(np.matmul(xT, ci), self.P[:, i])
-            self.Y[i] = np.linalg.solve(xT_ci_x + li, xT_ci_pi)
+            conf_samp = self.conf[:, i].T.toarray()
+            P = conf_samp.copy()
+            P[P > 0] = 1
+            ci = sparse.diags(conf_samp, [0]) + self.X_eye
+            xT_ci_x = xT.dot(ci).dot(self.X)
+            xT_ci_pi = xT.dot(ci).dot(P.T)
+            self.Y[i] = spsolve(xT_ci_x + self.lambda_eye, xT_ci_pi)
 
     def train(self):
-        for i in range(self.iteration):
+        predict_errors = []
+        confidence_errors = []
+        regularization_list = []
+        total_losses = []
+
+        for i in tqdm(range(self.iteration)):
             if i != 0:
                 self._optimize_user()
                 self._optimize_item()
-            predict = np.matmul(self.X, np.transpose(self.Y))
+            '''
+            predict = self.X.dot(self.Y.T)
+
             predict_error, confidence_error, regularization, total_loss = self._loss_function(predict)
 
-            self.predict_errors.append(predict_error)
-            self.confidence_errors.append(confidence_error)
-            self.regularization_list.append(regularization)
-            self.total_losses.append(total_loss)
+            predict_errors.append(predict_error)
+            confidence_errors.append(confidence_error)
+            regularization_list.append(regularization)
+            total_losses.append(total_loss)
 
             self.logger.debug('----------------step %d----------------' % i)
             self.logger.debug("predict error: %f" % predict_error)
             self.logger.debug("confidence error: %f" % confidence_error)
             self.logger.debug("regularization: %f" % regularization)
             self.logger.debug("total loss: %f" % total_loss)
+            '''
 
-        predict = np.matmul(self.X, np.transpose(self.Y))
-        return predict
+    def predict(self, userid, id_to_idx_mapping):
+        if userid not in id_to_idx_mapping.keys():  # If the ID is not included in the als model
+            return -1
+        idx = id_to_idx_mapping[userid]
+        return self.X[i].dot(self.Y.T)
