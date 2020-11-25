63357fada0995f5d1c21f73337988b20341e01ad
eppen <diffunity@gmail.com>
2020-11-25 19:23:37 +0900

    fix fb_utils.py bug

--- utils/fb_utils.py
+++ utils/fb_utils.py
@@ -4,15 +4,12 @@ import pandas as pd
 import numpy as np
 from scipy import sparse
 
-def to_sparse_tuples(data:json, user_n=0, item_n=0,
+def to_sparse_tuples(data:json,
                      user2idx:dict=None, item2idx:dict=None)\
                      -> (List[tuple], dict, dict):
-	if user_n > 0:
-        f = lambda x: len(x[1]) > user_n
-        data = dict(list(filter(f, data.items())))
     if user2idx is None:
-		user2idx = {j:i for i,j in enumerate(data.keys())}
-	if item2idx is None:
+        user2idx = {j:i for i,j in enumerate(data.keys())}
+    if item2idx is None:
         items = set()
         for i in list(data.values()):
             items = items.union(set(i))
@@ -31,5 +28,5 @@ def to_sparse(data:List[tuple], relevance:List[int]=None) -> sparse.csr_matrix:
     else:
         relevance = np.array(val)
     return sparse.csr_matrix((val, (data[:,0], data[:,1])),
-							 dtype="float32",
-							 shape=(data[:,0].max()+1, data[:,1].max()+1))
+                             dtype="float32",
+                             shape=(data[:,0].max()+1, data[:,1].max()+1))
