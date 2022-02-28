e8a83bf309c54781be744c76949568207db2b69a
SEUNGMIN <hawi75@yonsei.ac.kr>
2022-02-28 15:12:46 +0900

    Remove server config

--- conf/mongo_config.py
+++ conf/mongo_config.py
@@ -1,17 +1,17 @@
 DATABASE_CONFIG = {
     'server': {
-        'host': '10.41.167.152',
+        'host': '-',
         'port': 27017,
         'user': 'jolp',
-        'password': 'Whfvm1234',
+        'password': '-',
         'authSource': 'admin',
         'dbname': 'jolp'
     },
     'local': {
-        'host': '118.67.135.161',
+        'host': '-',
         'port': 27017,
         'user': 'jolp',
-        'password': 'Whfvm1234',
+        'password': '-',
         'authSource': 'admin',
         'dbname': 'jolp'
     }
